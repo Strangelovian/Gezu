4dea50a602cace46b699de103c1e6117f02cb2ed
strangelovian <strangelovian@gmail.com>
2017-10-21 22:34:03 +0200

    fix URL

--- packages/gzu/setup.py
+++ packages/gzu/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='gzu',
       version='0.1',
       description='',
-      url='http://github.com/strangelovian/gzu',
+      url='https://github.com/Strangelovian/Gezu',
       author='Strangelovian',
       author_email='strangelovian@gmail.com',
       license='MIT',
