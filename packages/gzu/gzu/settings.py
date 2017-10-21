14075432136b3c03121300e2a4077ef986ed2067
strangelovian <strangelovian@gmail.com>
2017-10-21 21:43:54 +0200

    revert to working script

--- packages/gzu/gzu/settings.py
+++ packages/gzu/gzu/settings.py
@@ -1,7 +1,7 @@
 import logging, logging.handlers, configparser, sys, os
 
-class Gzu:
 
+class Gzu:
     def init(rcfile):
         try:
             config = configparser.ConfigParser()
@@ -16,16 +16,17 @@ class Gzu:
 
             logfile = os.path.expanduser(config['log']['path'])
             splat = os.path.splitext(logfile)
-            handler = logging.handlers.RotatingFileHandler(splat[0] + ".debug" + splat[1], maxBytes=500*1024, backupCount=10)
+            handler = logging.handlers.RotatingFileHandler(splat[0] + ".debug" + splat[1], maxBytes=500 * 1024,
+                                                           backupCount=10)
             handler.setFormatter(formatter)
             handler.setLevel(logging.DEBUG)
             root.addHandler(handler)
 
-            handler = logging.handlers.RotatingFileHandler(logfile, maxBytes=50*1024, backupCount=10)
+            handler = logging.handlers.RotatingFileHandler(logfile, maxBytes=50 * 1024, backupCount=10)
             handler.setFormatter(formatter)
             handler.setLevel(logging.INFO)
             root.addHandler(handler)
 
         except:
-           print('init error:', sys.exc_info()[0])
-           raise
+            print('init error:', sys.exc_info()[0])
+            raise
